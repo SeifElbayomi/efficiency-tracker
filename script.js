c496d6cc4ba4fda95b5f6b826961afade59743b3
Seif Elbayomi <59777501+SeifElbayomi@users.noreply.github.com>
2023-12-03 17:32:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKPTUaF9QGEqG4xlEMBgfpH5eDs-2Myf4",
+  authDomain: "efficiecywa.firebaseapp.com",
+  projectId: "efficiecywa",
+  storageBucket: "efficiecywa.appspot.com",
+  messagingSenderId: "105355734656",
+  appId: "1:105355734656:web:09bcb954732fabba620065"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Reference to your database
+var database = firebase.database();
+
+// Write to the database
 function updateEfficiency(user) {
     let efficiency = document.getElementById(user + 'Efficiency').value;
-    document.getElementById(user + 'Display').textContent = 'Current: ' + efficiency;
+    database.ref('efficiencies/' + user).set({
+        efficiency: efficiency
+    });
 }
+
+// Read from the database and update UI in real-time
+database.ref('efficiencies/seif').on('value', (snapshot) => {
+    const data = snapshot.val();
+    document.getElementById('seifDisplay').textContent = 'Current: ' + (data ? data.efficiency : '-');
+});
+
+database.ref('efficiencies/hazem').on('value', (snapshot) => {
+    const data = snapshot.val();
+    document.getElementById('hazemDisplay').textContent = 'Current: ' + (data ? data.efficiency : '-');
+});
