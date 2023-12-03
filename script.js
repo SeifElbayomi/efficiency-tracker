980b4cab9acc76cfee7b849e24b7b907effb936e
Seif Elbayomi <59777501+SeifElbayomi@users.noreply.github.com>
2023-12-03 17:09:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-function submitEfficiency() {
-    var myEfficiency = document.getElementById('myEfficiency').value;
-    fetch('https://script.googleusercontent.com/macros/echo?user_content_key=pT0ve8btFJWTd-L3G2jDN4ZoeS93fM8NdVvrFcF1PWX8UEjI-EPzje-d0P3-OxzyGdoo1P7GQIbjahRb9jX4hNmzeYxy38_um5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBitC2CNq_CN9uJWQsT11mSZwGpyyRtJK623-lUK_quL4qKWM99rdXvCgnJfQ-gyNO_pcWFbEMCjAiaaLTJ82tE6aZHGLuhJYNz9Jw9Md8uu&lib=MtFKbjDwotP7HO9omSFYYqePLc0a1frJu', {
+function submitEfficiency(name) {
+    var efficiency = document.getElementById(name + 'Efficiency').value;
+    fetch('https://script.googleusercontent.com/macros/echo?user_content_key=xcBXwfb71vI08RXK6YTYQ24GdhcQg4veAdZ8SJsku9MO0kbglQ1N5fOJ36yY7d_2I1R25fhWBKwjySButadUsHAFCCPtswxqm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBitC2CNq_CN9uJWQsT11mSZwGpyyRtJK623-lUK_quL4qKWM99rdXvCgnJfQ-gyNO_pcWFbEMCjAiaaLTJ82tE6aZHGLuhJYNz9Jw9Md8uu&lib=MtFKbjDwotP7HO9omSFYYqePLc0a1frJu', {
         method: 'POST',
-        body: JSON.stringify({myEfficiency: myEfficiency, friendsEfficiency: ""}),
+        body: JSON.stringify({name: name, efficiency: efficiency}),
         headers: {
             'Content-Type': 'application/json'
         }
     });
 }
 
-function getFriendsEfficiency() {
-    fetch('https://script.googleusercontent.com/macros/echo?user_content_key=pT0ve8btFJWTd-L3G2jDN4ZoeS93fM8NdVvrFcF1PWX8UEjI-EPzje-d0P3-OxzyGdoo1P7GQIbjahRb9jX4hNmzeYxy38_um5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBitC2CNq_CN9uJWQsT11mSZwGpyyRtJK623-lUK_quL4qKWM99rdXvCgnJfQ-gyNO_pcWFbEMCjAiaaLTJ82tE6aZHGLuhJYNz9Jw9Md8uu&lib=MtFKbjDwotP7HO9omSFYYqePLc0a1frJu')
+function getEfficiencies() {
+    fetch('https://script.googleusercontent.com/macros/echo?user_content_key=xcBXwfb71vI08RXK6YTYQ24GdhcQg4veAdZ8SJsku9MO0kbglQ1N5fOJ36yY7d_2I1R25fhWBKwjySButadUsHAFCCPtswxqm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBitC2CNq_CN9uJWQsT11mSZwGpyyRtJK623-lUK_quL4qKWM99rdXvCgnJfQ-gyNO_pcWFbEMCjAiaaLTJ82tE6aZHGLuhJYNz9Jw9Md8uu&lib=MtFKbjDwotP7HO9omSFYYqePLc0a1frJu')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('friendsEfficiency').innerText = "Friend's Efficiency: " + data.friendsEfficiency;
+            document.getElementById('seifEfficiency').innerText = "Seif's Efficiency: " + data.seifEfficiency;
+            document.getElementById('hazemEfficiency').innerText = "Hazem's Efficiency: " + data.hazemEfficiency;
         });
 }
