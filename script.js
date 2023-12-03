072202b1ef93b4bc281644a7900bf71bbf1f5fcf
Seif Elbayomi <59777501+SeifElbayomi@users.noreply.github.com>
2023-12-03 17:25:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-function submitEfficiency(name) {
-    var efficiency = document.getElementById(name + 'Efficiency').value;
-    fetch('https://script.google.com/macros/s/AKfycbxIfq-c4QhWqlL5daprez5RLVRtBcGeNFA4HgowjMxaOxZ5XjUUkPVzX6r2sFSxnBDU/exec', {
-        method: 'POST',
-        body: JSON.stringify({name: name, efficiency: efficiency}),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-}
-
-function getEfficiencies() {
-    fetch('https://script.google.com/macros/s/AKfycbxIfq-c4QhWqlL5daprez5RLVRtBcGeNFA4HgowjMxaOxZ5XjUUkPVzX6r2sFSxnBDU/exec')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('seifEfficiency').innerText = "Seif's Efficiency: " + data.seifEfficiency;
-            document.getElementById('hazemEfficiency').innerText = "Hazem's Efficiency: " + data.hazemEfficiency;
-        });
+function updateEfficiency(user) {
+    let efficiency = document.getElementById(user + 'Efficiency').value;
+    document.getElementById(user + 'Display').textContent = 'Current: ' + efficiency;
 }
