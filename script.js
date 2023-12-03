aaa8679975040fd9af5ecf5e3b2195dcd779f612
Seif Elbayomi <59777501+SeifElbayomi@users.noreply.github.com>
2023-12-03 17:19:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function submitEfficiency(name) {
     var efficiency = document.getElementById(name + 'Efficiency').value;
-    fetch('https://script.google.com/macros/s/AKfycbylxiPMv_7OPprX9pu4N1qLNpDKbaHbjHdjReDmyfyTgspFDAi104dEy6bHOQi1f4Faug/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbxIfq-c4QhWqlL5daprez5RLVRtBcGeNFA4HgowjMxaOxZ5XjUUkPVzX6r2sFSxnBDU/exec', {
         method: 'POST',
         body: JSON.stringify({name: name, efficiency: efficiency}),
         headers: {
@@ -10,7 +10,7 @@ function submitEfficiency(name) {
 }
 
 function getEfficiencies() {
-    fetch('https://script.google.com/macros/s/AKfycbylxiPMv_7OPprX9pu4N1qLNpDKbaHbjHdjReDmyfyTgspFDAi104dEy6bHOQi1f4Faug/exec')
+    fetch('https://script.google.com/macros/s/AKfycbxIfq-c4QhWqlL5daprez5RLVRtBcGeNFA4HgowjMxaOxZ5XjUUkPVzX6r2sFSxnBDU/exec')
         .then(response => response.json())
         .then(data => {
             document.getElementById('seifEfficiency').innerText = "Seif's Efficiency: " + data.seifEfficiency;
