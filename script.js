0d466a7829b479abf37bba4dc4140ce7f85d763b
Seif Elbayomi <59777501+SeifElbayomi@users.noreply.github.com>
2023-12-03 17:14:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function submitEfficiency(name) {
     var efficiency = document.getElementById(name + 'Efficiency').value;
-    fetch('https://script.googleusercontent.com/macros/echo?user_content_key=xcBXwfb71vI08RXK6YTYQ24GdhcQg4veAdZ8SJsku9MO0kbglQ1N5fOJ36yY7d_2I1R25fhWBKwjySButadUsHAFCCPtswxqm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBitC2CNq_CN9uJWQsT11mSZwGpyyRtJK623-lUK_quL4qKWM99rdXvCgnJfQ-gyNO_pcWFbEMCjAiaaLTJ82tE6aZHGLuhJYNz9Jw9Md8uu&lib=MtFKbjDwotP7HO9omSFYYqePLc0a1frJu', {
+    fetch('https://script.google.com/macros/s/AKfycbylxiPMv_7OPprX9pu4N1qLNpDKbaHbjHdjReDmyfyTgspFDAi104dEy6bHOQi1f4Faug/exec', {
         method: 'POST',
         body: JSON.stringify({name: name, efficiency: efficiency}),
         headers: {
@@ -10,7 +10,7 @@ function submitEfficiency(name) {
 }
 
 function getEfficiencies() {
-    fetch('https://script.googleusercontent.com/macros/echo?user_content_key=xcBXwfb71vI08RXK6YTYQ24GdhcQg4veAdZ8SJsku9MO0kbglQ1N5fOJ36yY7d_2I1R25fhWBKwjySButadUsHAFCCPtswxqm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBitC2CNq_CN9uJWQsT11mSZwGpyyRtJK623-lUK_quL4qKWM99rdXvCgnJfQ-gyNO_pcWFbEMCjAiaaLTJ82tE6aZHGLuhJYNz9Jw9Md8uu&lib=MtFKbjDwotP7HO9omSFYYqePLc0a1frJu')
+    fetch('https://script.google.com/macros/s/AKfycbylxiPMv_7OPprX9pu4N1qLNpDKbaHbjHdjReDmyfyTgspFDAi104dEy6bHOQi1f4Faug/exec')
         .then(response => response.json())
         .then(data => {
             document.getElementById('seifEfficiency').innerText = "Seif's Efficiency: " + data.seifEfficiency;
